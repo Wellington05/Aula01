75fc5f5c15d9b1268cc9977653674dbcb821fa85
aula Usuario de aula <aula@setrem.com.br>
2021-09-04 09:19:40 -0300

    primeiro commit

---
+++ Calcular masa e altura/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calcular_masa_e_altura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(" Calcular peso/altura²");
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine(" Informe a peso (kg): ");
+
+            int peso = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" Informe a altura (m): ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            calcularImc(peso, altura);
+        }
+
+        static void calcularImc(int peso, double altura)
+        { 
+           double imc = peso /(altura * altura);
+            Console.WriteLine("Imc de " + imc);
+
+        }
+    }
+}
